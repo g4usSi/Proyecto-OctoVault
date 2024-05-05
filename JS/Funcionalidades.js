e3c24904b71c9cc8a17bb670ba59be5315dc9833
g4usSi <geovanny.alcon2@gmail.com>
2024-05-05 12:03:16 -0600

    Delimitacin de Espacios en el archivo de javascript

---
+++ JS/Funcionalidades.js
@@ -0,0 +1,15 @@
+/*Leonor - Trama/Sinopsis*/
+
+/*Leonor - Historia*/
+
+/*Jenny - Estetica*/
+
+/*Mily - Personajes*/
+
+/*Daniel - Impacto Cultural*/
+
+/*Geo - Curiosidades y Teorias*/
+
+/*Miguel - Memes*/
+
+/*Extra-testing*/
