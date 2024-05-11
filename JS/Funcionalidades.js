c75e1f701279d7e1a0a18eb39e16fdc166b2c75f
Jennifer Saquic <jenntecum2001@gmail.com>
2024-05-10 20:30:34 -0600

    cambios

--- JS/Funcionalidades.js
+++ JS/Funcionalidades.js
@@ -1,3 +1,15 @@
+/*Funciones*/
+const menuItems = document.querySelectorAll('nav ul li a');
+
+menuItems.forEach(menuItem => {
+  menuItem.addEventListener('click', function(event) {
+    event.preventDefault(); // Evita el comportamiento predeterminado del enlace
+    const targetSectionId = this.getAttribute('href'); // Obtiene el ID de la sección
+    const targetSection = document.querySelector(targetSectionId); // Obtiene la referencia a la sección
+    targetSection.scrollIntoView({ behavior: 'smooth' }); // Desplazamiento suave
+  });
+});
+
 /*Leonor - Trama/Sinopsis*/
 
 /*Leonor - Historia*/
